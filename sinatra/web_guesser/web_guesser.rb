5cb44a36b8310c44700c94a28b29a522ccbf0125
Jay Burbyga <jayburbyga@gmail.com>
2019-08-21 20:08:09 -0400

    I1: generates a random number

--- sinatra/web_guesser/web_guesser.rb
+++ sinatra/web_guesser/web_guesser.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+
+def generate_rand_num(max_num)
+  rand(max_num + 1)
+end
+
+
+secret_num = generate_rand_num(101)
 
 get '/' do 
-  'Hello World'
+  "The secret number is #{secret_num}"  
 end
