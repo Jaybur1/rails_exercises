278884210ad02b9ab4407974c6c99aa2ed499c0c
Jay Burbyga <jayburbyga@gmail.com>
2019-08-23 14:47:51 -0400

    I3: number guessing game complete

--- sinatra/web_guesser/web_guesser.rb
+++ sinatra/web_guesser/web_guesser.rb
@@ -2,13 +2,26 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-def generate_rand_num(max_num)
-  rand(max_num + 1)
+set :generate_rand_num , Proc.new {rand(1..100)}
+SECRET_NUM = settings.generate_rand_num
+
+
+def check_guess(guess)
+  return "Give me a number" if guess == 0
+
+  return "NOPE, TO HIGH" if guess > SECRET_NUM
+  return "NOPE, TO LOW" if guess < SECRET_NUM
+  return "YES!!! THAT'S IS CORRECT" if guess == SECRET_NUM
+  
 end
-
-
-secret_num = generate_rand_num(101)
-
+ 
 get '/' do 
-  erb :index, locals: {secret_num: secret_num}
+  user_guess = params["guess"].to_i
+  
+  output = check_guess(user_guess)
+  erb :index, locals: {
+    secret_num: SECRET_NUM,
+    output: output
+  }
+  #throw params.inspect
 end
