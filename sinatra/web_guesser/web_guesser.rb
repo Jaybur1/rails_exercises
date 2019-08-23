5be6b71aacb81eef5254286f719a69c2cddbc013
Jay Burbyga <jayburbyga@gmail.com>
2019-08-23 15:31:39 -0400

    I4: color function added

--- sinatra/web_guesser/web_guesser.rb
+++ sinatra/web_guesser/web_guesser.rb
@@ -3,8 +3,21 @@ require 'sinatra/reloader'
 
 
 set :generate_rand_num , Proc.new {rand(1..100)}
+set :new_game, Proc.new {SECRET_NUM = settings.generate_rand_num}
 SECRET_NUM = settings.generate_rand_num
 
+def background(guess)
+  return "white" if guess == 0
+  
+  if guess == SECRET_NUM
+    return 'green'
+  elsif guess.between?((SECRET_NUM - 5),SECRET_NUM) || guess.between?(SECRET_NUM,(SECRET_NUM + 5))
+    return 'lightred'
+  elsif guess < (SECRET_NUM - 5) || guess > (SECRET_NUM + 5)
+    return 'red'
+  
+  end
+end
 
 def check_guess(guess)
   return "Give me a number" if guess == 0
@@ -21,7 +34,9 @@ get '/' do
   output = check_guess(user_guess)
   erb :index, locals: {
     secret_num: SECRET_NUM,
-    output: output
+    output: output,
+    background: background(user_guess.to_i),
+  
   }
   #throw params.inspect
 end
